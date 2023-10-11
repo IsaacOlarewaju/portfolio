c79110b1d6163d087588a6ee6fbab7354fa2aa07
Isaac Olarewaju <isaacolarewaju@Isaacs-iMac.local>
2023-10-11 22:48:53 +0100

    I added some comments

--- src/index.js
+++ src/index.js
@@ -3,13 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  Link,
+  RouterProvider,
+} from "react-router-dom";
+import { PortfolioComponent } from './Components/portfolio';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+  {
+    path: "/portfolio",
+    element: <PortfolioComponent />,
+  },
+
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
